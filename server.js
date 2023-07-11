e1883bcdda9af9163196ee15d7740b49d1b77965
hd9 <hectorjr_805@yahoo.com>
2023-07-10 22:27:08 -0700

    api route

--- server.js
+++ server.js
@@ -21,7 +21,17 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// GET /index.html route
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
+// GET /api route
+app.get("/api/notes", (req, res) => {
+  readFile("db/db.json").then((data) => {
+    res.send(data);
+  });
+});
 
 // POST /api/notes route
 app.post("/api/notes", (req, res) => {
